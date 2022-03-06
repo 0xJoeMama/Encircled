1b89d54ddffbcd9b3bc7d9bbe034183373157c1c
0xJoeMama <0xjoemama@gmail.com>
2022-03-06 20:12:02 +0200

    [build.gradle, settings.gradle] Moved to using the gradle kotlin dsl since its more concise

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("fabric-loom") version "0.9-SNAPSHOT"
+}
+
+val archiveBaseName = property("archives_base_name")!!
+group = property("maven_group")!!
+version = property("mod_version")!!
+
+val mcVersion = property("minecraft_version")!!
+val yarnVersion = property("yarn_mappings")!!
+val loaderVersion = property("loader_version")!!
+val apiVersion = property("fabric_version")!!
+
+dependencies {
+    minecraft("com.mojang:minecraft:${mcVersion}")
+    mappings("net.fabricmc:yarn:${yarnVersion}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${apiVersion}")
+}
+
+tasks {
+    withType(ProcessResources::class) {
+        inputs.property("version", version)
+        filesMatching("fabric.mod.json") {
+            expand("version" to version)
+        }
+    }
+
+    withType(Jar::class) {
+        from("LICENSE") {
+            rename { "${it}_${archiveBaseName}" }
+        }
+    }
+
+    withType(JavaCompile::class) {
+        options.release.set(17)
+        options.encoding = "UTF-8"
+    }
+}
+
+java {
+    withSourcesJar()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "VoidMagic-New"
+
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        gradlePluginPortal()
+    }
+}
